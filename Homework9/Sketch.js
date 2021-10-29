917417f759c1659822c53b616de702bced58b4b4
GordonMcMillion <89866627+GordonMcMillion@users.noreply.github.com>
2021-10-28 19:34:57 -0600

    Create Sketch.js

---
+++ Homework9/Sketch.js
@@ -0,0 +1,33 @@
+function setup()
+            {
+                createCanvas(500,600);
+            }
+
+            function draw()
+            {
+                background(120);
+                textSize(22)
+                text("the scream", 10,80);
+
+                line(130,175,175,250);
+                line(325,250,360,175);
+
+                fill('#fae');
+                circle(250,350,200);
+                fill(30,25,120);
+                rect(150,450,200);
+                ellipse(145,400,30,200);
+                ellipse(345,400,30,200);
+                fill('#222222');
+                circle(225,320,20);
+                circle(275,320,20);
+                fill(51);
+                ellipse(250,375,50,60);
+                
+               
+
+                fill('rgb(0,255,0)');
+                textSize(17);
+                text("Gordon McMillion",200,500 );
+
+            }
